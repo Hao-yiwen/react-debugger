1c02562e45ff772976c6465951859b902fa49e23
yw.hao <yw.hao@trip.com>
2024-07-22 19:05:06 +0800

    feat: add test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
 
-function App() {
+// 第一层组件
+const FirstLevelComponent = () => {
+  const [firstLevelState, setFirstLevelState] = useState("1111111111");
+
+  useEffect(() => {
+    console.time("FirstLevelComponent render time");
+    return () => {
+      console.timeEnd("FirstLevelComponent render time");
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>First Level Component</h1>
+      <p>{firstLevelState}</p>
+      <SecondLevelComponent setFirstLevelState={setFirstLevelState} />
     </div>
   );
-}
+};
+
+// 第二层组件
+const SecondLevelComponent = ({ setFirstLevelState }) => {
+  const [secondLevelState, setSecondLevelState] = useState("1111111111");
+
+  const updateFirstLevelState = useCallback(() => {
+    setSecondLevelState("2222222222");
+    setFirstLevelState("2222222222");
+  }, [setFirstLevelState]);
+
+  useEffect(() => {
+    console.time("SecondLevelComponent render time");
+    return () => {
+      console.timeEnd("SecondLevelComponent render time");
+    };
+  });
+
+  return (
+    <div>
+      <h2>Second Level Component</h2>
+      <p>{secondLevelState}</p>
+      <ThirdLevelComponent setSecondLevelState={updateFirstLevelState} />
+    </div>
+  );
+};
+
+// 第三层组件
+const ThirdLevelComponent = ({ setSecondLevelState }) => {
+  const [thirdLevelState, setThirdLevelState] = useState("11111");
+
+  const handleClick = () => {
+    // 更新第三层组件的 state
+    setTimeout(() => {
+      setThirdLevelState("33333333");
+    }, 1000);
+    setThirdLevelState("2222222");
+
+    // 同时触发第二层组件的 hook
+    setSecondLevelState("2222222");
+  };
+
+  useEffect(() => {
+    console.time("ThirdLevelComponent render time");
+    return () => {
+      console.timeEnd("ThirdLevelComponent render time");
+    };
+  });
+
+  return (
+    <div>
+      <h3>Third Level Component</h3>
+      <p>{thirdLevelState}</p>
+      <button onClick={handleClick}>Update State</button>
+    </div>
+  );
+};
+
+// 渲染第一层组件
+const App = () => {
+  return (
+    <div>
+      <FirstLevelComponent />
+    </div>
+  );
+};
 
 export default App;
