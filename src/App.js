9dd03b1b3e5be00b388b68ec6c5ab12aaa76bf84
yw.hao <yw.hao@trip.com>
2024-07-23 20:54:59 +0800

    feat: add test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
+
+// 模拟复杂异步操作
+const simulateAsyncOperation = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("done");
+    }, 500); // 模拟500ms的异步操作
+  });
+};
 
 // 第一层组件
 const FirstLevelComponent = () => {
   const [firstLevelState, setFirstLevelState] = useState("1111111111");
 
   useEffect(() => {
-    console.time("FirstLevelComponent render time");
+    console.log(performance.now() + ' FirstLevelComponent render time');
     return () => {
-      console.timeEnd("FirstLevelComponent render time");
+      console.log(performance.now() + ' FirstLevelComponent remove render time');
     };
   });
 
@@ -23,16 +32,30 @@ const FirstLevelComponent = () => {
 // 第二层组件
 const SecondLevelComponent = ({ setFirstLevelState }) => {
   const [secondLevelState, setSecondLevelState] = useState("1111111111");
+  const thirdInputRef = useRef(null);
+  const thirdViewRef = useRef(null);
+  const [isShowThirdLevel, setIsShowThirdLevel] = useState(true);
 
-  const updateFirstLevelState = useCallback(() => {
-    setSecondLevelState("2222222222");
-    setFirstLevelState("2222222222");
-  }, [setFirstLevelState]);
+  const updateFirstLevelState = useCallback(
+    async (ref, viewRef) => {
+      thirdInputRef.current = ref;
+      thirdViewRef.current = viewRef;
+      setSecondLevelState("2222222222");
+      await simulateAsyncOperation();
+      thirdInputRef.current.focus();
+      thirdViewRef.current.style.backgroundColor = "red";
+      setFirstLevelState("2222222222");
+      setIsShowThirdLevel(false);
+    },
+    [setFirstLevelState]
+  );
 
   useEffect(() => {
-    console.time("SecondLevelComponent render time");
+    console.log(performance.now() + " SecondLevelComponent render time");
     return () => {
-      console.timeEnd("SecondLevelComponent render time");
+      console.log(
+        performance.now() + " SecondLevelComponent remove render time"
+      );
     };
   });
 
@@ -40,7 +63,7 @@ const SecondLevelComponent = ({ setFirstLevelState }) => {
     <div>
       <h2>Second Level Component</h2>
       <p>{secondLevelState}</p>
-      <ThirdLevelComponent setSecondLevelState={updateFirstLevelState} />
+      {isShowThirdLevel && <ThirdLevelComponent setSecondLevelState={updateFirstLevelState} />}
     </div>
   );
 };
@@ -48,29 +71,33 @@ const SecondLevelComponent = ({ setFirstLevelState }) => {
 // 第三层组件
 const ThirdLevelComponent = ({ setSecondLevelState }) => {
   const [thirdLevelState, setThirdLevelState] = useState("11111");
+  const inputRef = useRef(null);
+  const viewRef = useRef(null);
 
   const handleClick = () => {
     // 更新第三层组件的 state
-    setTimeout(() => {
-      setThirdLevelState("33333333");
-    }, 1000);
     setThirdLevelState("2222222");
 
     // 同时触发第二层组件的 hook
-    setSecondLevelState("2222222");
+    setTimeout(() => {
+      setSecondLevelState(inputRef.current, viewRef.current);
+    }, 100);
   };
 
   useEffect(() => {
-    console.time("ThirdLevelComponent render time");
+    console.log(performance.now() + " ThirdLevelComponent render time");
     return () => {
-      console.timeEnd("ThirdLevelComponent render time");
+      console.log(
+        performance.now() + " ThirdLevelComponent remove render time"
+      );
     };
   });
 
   return (
-    <div>
+    <div ref={viewRef}>
       <h3>Third Level Component</h3>
       <p>{thirdLevelState}</p>
+      <input placeholder="focus me" ref={inputRef} />
       <button onClick={handleClick}>Update State</button>
     </div>
   );
